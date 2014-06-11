bab3db677d86258a0775fba9261823319f682d4a
Wen Kokke <wenkokke@users.noreply.github.com>
2014-06-11 13:37:24 +0200

    Added convenience methods to the expression builder.

--- src/main/java/lambdacalc/impl/IExprBuilder.java
+++ src/main/java/lambdacalc/impl/IExprBuilder.java
@@ -4,6 +4,7 @@ import static lombok.AccessLevel.PRIVATE;
 import lambdacalc.Expr;
 import lambdacalc.ExprBuilder;
 import lambdacalc.Symbol;
+import lambdacalc.Type;
 import lambdacalc.impl.IExpr.IAbstraction;
 import lambdacalc.impl.IExpr.IApplication;
 import lambdacalc.impl.IExpr.IVariable;
@@ -15,17 +16,25 @@ import lombok.experimental.FieldDefaults;
 public class IExprBuilder implements ExprBuilder {
 
 	@Override
-	public Expr abstraction(Symbol arg0, Expr arg1) {
+	public final Expr abstraction(Symbol arg0, Expr arg1) {
 		return new IAbstraction(arg0,arg1);
+	}
+	
+	public final Expr abstraction(String name, Type type, Expr body) {
+		return new IAbstraction(new ISymbol(name, type), body);
 	}
 
 	@Override
-	public Expr application(Expr arg0, Expr arg1) {
+	public final Expr application(Expr arg0, Expr arg1) {
 		return new IApplication(arg0,arg1);
 	}
 
 	@Override
-	public Expr variable(Symbol s) {
+	public final Expr variable(Symbol s) {
 		return new IVariable(s);
+	}
+	
+	public final Expr variable(String name, Type type) {
+		return new IVariable(new ISymbol(name, type));
 	}
 }
